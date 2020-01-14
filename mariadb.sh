01950d9798ed7e6ee8f5c4051ae159aa3cd72c95
Vincent Milum Jr <git@darkain.com>
2020-01-14 15:08:09 -0800

    Automatically enable MariaDB service

--- mariadb.sh
+++ mariadb.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+
 pkg install -y mariadb104-server mariadb104-client galera rsync stunnel grc bash
+
+LINE='mysql_enable="YES"'
+FILE='/etc/rc.conf.local'
+touch "$FILE"
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+service mysql-server start
+
+mysql -e "DROP DATABASE IF EXISTS test"
+mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+mysql -e "DELETE FROM mysql.global_priv WHERE User=''"
+mysql -e "DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+mysql -e "FLUSH PRIVILEGES"
