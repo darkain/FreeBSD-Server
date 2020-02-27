54528bda3b734faa9bc599e164db1e2fb7bc9e6d
Vincent Milum Jr <git@darkain.com>
2020-02-26 18:48:37 -0800

    Updating dependencies and making list more managable

--- altaform.sh
+++ altaform.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-pkg install -y lighttpd php74 php74-curl php74-exif php74-sodium php74-zip php74-zlib php74-mysqli php74-pecl-imagick php74-pecl-redis php74-sysvmsg php74-sysvsem php74-sysvshm php74-sockets php74-simplexml php74-extensions php74-soap php74-openssl
+pkg install -y \
+lighttpd \
+php74 \
+php74-ctype \
+php74-curl \
+php74-exif \
+php74-extensions \
+php74-filter \
+php74-mysqli \
+php74-opcache \
+php74-openssl \
+php74-pecl-imagick \
+php74-pecl-redis \
+php74-simplexml \
+php74-soap \
+php74-sockets \
+php74-sodium \
+php74-sqlite3 \
+php74-sysvmsg \
+php74-sysvsem \
+php74-sysvshm \
+php74-tokenizer \
+php74-xml \
+php74-xmlreader \
+php74-xmlwriter \
+php74-zip \
+php74-zlib
