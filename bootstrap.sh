18561cdd8d0a396d911175339aa10af41e437425
Vincent Milum Jr <git@darkain.com>
2020-04-01 16:02:46 -0700

    switch to HTTPS after we have root CAs installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,9 +35,6 @@ elif [ `which apt 2>/dev/null` ]; then
 # INSTALL GIT ON FREEBSD
 elif [ `which pkg 2>/dev/null` ]; then
 
-	# FORCE HTTPS INSTEAD OF HTTP
-	sed -i '' 's/http:/https:/' /etc/pkg/FreeBSD.conf
-
 	# USE LATEST REPOSITORY (X86-64 ONLY, BECAUSE THIS IS BROKEN ON ARM)
 	if [ `uname -m` = 'amd64' ]; then
 		sed -i '' 's/quarterly"/latest"/' /etc/pkg/FreeBSD.conf
@@ -54,7 +51,10 @@ elif [ `which pkg 2>/dev/null` ]; then
 	if `pkg info git-lite >/dev/null 2>&1`; then
 		pkg remove -y git-lite
 	fi
-	pkg install -y git
+	pkg install -y git ca_root_nss
+
+	# FORCE HTTPS INSTEAD OF HTTP
+	sed -i '' 's/http:/https:/' /etc/pkg/FreeBSD.conf
 
 # ERROR, CANNOT CONTINUE
 else
