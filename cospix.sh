8c2f97711b9562bcc587b84ee2d93b10357e061b
Vincent Milum Jr <git@darkain.com>
2022-06-04 17:41:59 -0700

    Update cospix.sh

--- cospix.sh
+++ cospix.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# MAKE SURE ALL PACKAGES ARE INSTALLED FIRST
+sh altaform.sh
+
+# MOVE TO OUR WORKING DIRECTORY
 cd /var/www
 
 # GET THE INITIAL SOURCE CODE
@@ -11,3 +15,6 @@ git config --global --add safe.directory /var/www
 # PULL ALL THE SUBMODULES
 git submodule init
 git submodule update
+
+# SET OWNER TO WWW USER
+chown -R www:www /var/www
