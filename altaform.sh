c253ae4308134260f1e7f47b450bdb9fc0838a1c
Vincent Milum Jr <git@darkain.com>
2022-06-04 17:04:33 -0700

    Updating for PHP 8.1

--- altaform.sh
+++ altaform.sh
@@ -2,28 +2,32 @@
 
 pkg install -y \
 lighttpd \
-php74 \
-php74-ctype \
-php74-curl \
-php74-exif \
-php74-extensions \
-php74-filter \
-php74-mysqli \
-php74-opcache \
-php74-openssl \
-php74-pecl-imagick \
-php74-pecl-redis \
-php74-simplexml \
-php74-soap \
-php74-sockets \
-php74-sodium \
-php74-sqlite3 \
-php74-sysvmsg \
-php74-sysvsem \
-php74-sysvshm \
-php74-tokenizer \
-php74-xml \
-php74-xmlreader \
-php74-xmlwriter \
-php74-zip \
-php74-zlib
+php81 \
+php81-ctype \
+php81-curl \
+php81-exif \
+php81-extensions \
+php81-filter \
+php81-mysqli \
+php81-opcache \
+php81-pecl-imagick \
+php81-pecl-redis \
+php81-simplexml \
+php81-session \
+php81-soap \
+php81-sockets \
+php81-sodium \
+php81-sqlite3 \
+php81-tokenizer \
+php81-xml \
+php81-xmlreader \
+php81-xmlwriter \
+php81-zip \
+php81-zlib
+
+# THESE ARE OPTIONAL, CURRENTLY TURNING THEM OFF
+# USAGE OF THESE WILL MOST LIKELY BE REMOVED FROM PUDL
+# THEY ARE NOT USED ANYWHERE ELSE IN ALTAFORM OR OTHER DEPENDENCIES
+# php74-sysvmsg \
+# php74-sysvsem \
+# php74-sysvshm \
