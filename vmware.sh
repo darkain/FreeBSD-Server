463091168f614b0dfb660ef2c8d5c0a66e134845
Vincent Milum Jr <git@darkain.com>
2022-06-18 09:32:55 -0700

    Update vmware.sh

--- vmware.sh
+++ vmware.sh
@@ -3,11 +3,12 @@
 # INSTALL OPEN-VM-TOOLS
 pkg install -y open-vm-tools-nox11
 
-# ENABLE OPEN-VM-TOOLS SERVICE
+# ENABLE OPEN-VM-TOOLS SERVICES
 sysrc vmware_guest_vmmemctl_enable="YES"
 sysrc vmware_guest_vmxnet_enable="YES"
 sysrc vmware_guest_kmod_enable="YES"
 sysrc vmware_guestd_enable="YES"
 
-# START OPEN-VM-TOOLS SERVICE
+# START OPEN-VM-TOOLS SERVICES
 service vmware-guestd start
+service vmware-kmod start
