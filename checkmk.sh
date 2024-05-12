18454c57c8c8f58b99058a4124bf0dfd48cdd080
Vincent Milum Jr <git@darkain.com>
2024-05-12 10:21:09 -0700

    Update checkmk.sh

--- checkmk.sh
+++ checkmk.sh
@@ -13,6 +13,17 @@ LINE='checkmk-agent stream tcp nowait root /usr/local/bin/check_mk_agent check_m
 FILE='/etc/inetd.conf'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
+# Create missing folders
+mkdir /usr/local/etc/check_mk
+mkdir /usr/local/lib/check_mk_agent
+mkdir /usr/local/lib/check_mk_agent/local
+mkdir /usr/local/lib/check_mk_agent/plugins
+ln -s /usr/local/etc/check_mk /etc/check_mk
+ln -s /usr/local/lib/check_mk_agent /var/lib/check_mk_agent
+
+# Fetch my "fixed" check_mk_agent file
+fetch -o /usr/local/bin/check_mk_agent https://raw.githubusercontent.com/darkain/checkmk/master/agents/check_mk_agent.freebsd
+
 # Enable and start inetd, thus starting Checkmk agent
 sysrc inetd_enable="YES"
 service inetd restart
