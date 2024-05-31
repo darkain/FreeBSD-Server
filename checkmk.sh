a3942abb8ad27e1ad1e7f34104cc1b40d1e7a706
Vincent Milum Jr <git@darkain.com>
2024-05-31 00:19:13 -0700

    Ensure check_mk_agent is executable

--- checkmk.sh
+++ checkmk.sh
@@ -26,6 +26,8 @@ ln -s /usr/local/lib/check_mk_agent /var/lib/check_mk_agent
 
 # Fetch my "fixed" check_mk_agent file
 fetch -o /usr/local/bin/check_mk_agent https://raw.githubusercontent.com/darkain/checkmk/master/agents/check_mk_agent.freebsd
+chmod +x /usr/local/bin/check_mk_agent
+
 
 # Enable and start inetd, thus starting Checkmk agent
 sysrc inetd_enable="YES"
