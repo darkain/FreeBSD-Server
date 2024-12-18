be0905bc48ae5dd8aaebfe41bea446da7ea969c8
Vincent Milum Jr <git@darkain.com>
2024-12-17 23:28:10 -0800

    Create pkgbase.sh
    Initial support for FreeBSD's new "pkgbase" replacing "freebsd-update"

---
+++ pkgbase.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+
+# REPO CONFIG NEEDS A HOME IF IT DOESN'T EXIST
+mkdir -p /usr/local/etc/pkg/repos/
+
+
+
+
+# CREATE NEW PKG CONF FOR PKGBASE
+cat <<'EOF' >/usr/local/etc/pkg/repos/FreeBSD-base.conf
+FreeBSD-base: {
+  url: "pkg+https://pkg.FreeBSD.org/${ABI}/base_release_2",
+  mirror_type: "srv",
+  signature_type: "fingerprints",
+  fingerprints: "/usr/share/keys/pkg",
+  enabled: yes
+}
+EOF
+
+
+
+
+# UPDATE PKG WITH NEW REPO
+pkg update
+
+
+
+
+# INSTALL PKGBASE
+pkg search -r FreeBSD-base BSD | \
+awk '!/-(dbg|man|dev|lib32|src|kernel)-/' | \
+sed 's/-[0-9][0-9]\.[0-9].*//' | \
+xargs pkg install -y -r FreeBSD-base FreeBSD-kernel-generic
+
+
+
+
+# RESTORE IMPORTANT FILES
+cp /etc/pkg/FreeBSD.conf.pkgsave /etc/pkg/FreeBSD.conf
+cp /etc/group.pkgsave /etc/group
+cp /etc/master.passwd.pkgsave /etc/master.passwd
+cp /etc/shells.pkgsave /etc/shells
+cp /etc/sysctl.conf.pkgsave /etc/sysctl.conf
+
+
+
+
+# CLEAN UP OLD FILES
+find / -name "*.pkgsave" | xargs rm -f
