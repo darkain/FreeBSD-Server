f77251f321638ab45c15ba38e31b3d4572a7c0b2
Vincent Milum Jr <git@darkain.com>
2025-04-03 22:19:45 -0700

    Update pkgbase.sh
    Fix SSH config getting overwritten by pkgbase

--- pkgbase.sh
+++ pkgbase.sh
@@ -64,3 +64,10 @@ find / -name "*.pkgsave" | xargs rm -f
 ! rm /root/.cshrc 2>/dev/null
 ln -s /vince/root/.cshrc /root/
 chsh -s /bin/csh root
+
+
+
+
+# RE-ENABLE / FIX SSH
+! rm /etc/ssh/sshd_config 2>/dev/null
+ln -s /vince/etc/ssh/sshd_config /etc/ssh/
