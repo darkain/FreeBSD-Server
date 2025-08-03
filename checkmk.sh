1d166284617fd570e3ba30109f0ffbc8c8fc4a98
Vincent Milum Jr <git@darkain.com>
2025-08-02 20:03:07 -0700

    Adding "pkg audit" checkmk plugin

--- checkmk.sh
+++ checkmk.sh
@@ -3,6 +3,7 @@
 # For OPNsense, use this instead:
 # https://github.com/bashclub/checkmk-opnsense-agent
 
+
 # Install Checkmk Agent and some extra things for it to monitor
 if [ `uname -i` = 'FREENAS64' -o `uname -i` = 'TRUENAS' ]; then
 	# Do nothing on FreeNAS/TrueNAS
@@ -12,15 +13,18 @@ else
 	pkg install -y python ipmitool libstatgrab check_mk_agent
 fi
 
+
 # Register Checkmk TCP Port
 LINE='checkmk-agent 6556/tcp #Checkmk monitoring agent'
 FILE='/etc/services'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
+
 # Register Checkmk with inetd
 LINE='checkmk-agent stream tcp nowait root /usr/local/bin/check_mk_agent check_mk_agent'
 FILE='/etc/inetd.conf'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
 
 # Create missing folders
 mkdir /usr/local/etc/check_mk
@@ -30,7 +34,12 @@ mkdir /usr/local/lib/check_mk_agent/plugins
 ln -s /usr/local/etc/check_mk /etc/check_mk
 ln -s /usr/local/lib/check_mk_agent /var/lib/check_mk_agent
 
-# Fetch my "fixed" check_mk_agent file
+
+# Install "pkg audit" plugin
+ln -s /vince/usr/local/lib/check_mk_agent/plugins/pkg_audit.py /usr/local/lib/check_mk_agent/plugins/
+
+
+# Fetch latest check_mk_agent file
 fetch -o /usr/local/bin/check_mk_agent https://raw.githubusercontent.com/Checkmk/checkmk/master/agents/check_mk_agent.freebsd
 chmod +x /usr/local/bin/check_mk_agent
 
