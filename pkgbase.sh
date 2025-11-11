2428fbc141b9f2e8afb6bfffdcc5e8be22d468ba
Vincent Milum Jr <git@darkain.com>
2025-11-10 22:10:53 -0800

    Fix root shell and SSH configuration
    Updated root shell configuration and SSH setup.

--- pkgbase.sh
+++ pkgbase.sh
@@ -28,18 +28,8 @@ vipw
 
 
 
-# FIX ROOT SHELL
-! rm /root/.cshrc 2>/dev/null
-ln -s /vince/root/.cshrc /root/
-chsh -s /bin/csh root
-
-
-
-
-# RE-ENABLE / FIX SSH
-! rm /etc/ssh/sshd_config 2>/dev/null
-ln -s /vince/etc/ssh/sshd_config /etc/ssh/
-service sshd restart
+# FIX ROOT SHELL AND SSH
+sh /vince/symlink.sh
 
 
 
