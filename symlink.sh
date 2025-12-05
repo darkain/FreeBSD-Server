09ab94a23f758b755b0e35be5ef43eba8c09f0b9
Vincent Milum Jr <git@darkain.com>
2025-12-05 00:34:18 -0800

    Enhance symlink.sh to manage symlinks and deletions
    Added functionality to fix symlinks and delete extra files in the root directory.

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,24 @@
-# FIX ROOT SHELL
-if [ ! -L "/root/.cshrc" ]; then
-	! rm /root/.cshrc 2>/dev/null
-	ln -s /vince/root/.cshrc /root/
-	chsh -s /bin/csh root
-fi
+symlink=".cshrc .nanorc .my.cnf .grcat .login"
+delete=".tcshrc"
+
+
+# FIX SYMLINKS
+for item in $symlink; do
+	if [ ! -L "/root/$item" ]; then
+		echo "Fixing: /root/$item"
+		! rm "/root/$item" 2>/dev/null
+		ln -s "/vince/root/$item" /root/
+	fi
+done
+
+
+# DELETE EXTRA FILES
+for item in $delete; do
+	if [ -f "/root/$item" ]; then
+		echo "Deleting: /root/$item"
+		! rm "/root/$item" 2>/dev/null
+	fi
+done
 
 
 
