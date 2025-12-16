135a26c3554e55f37b22304c8ba397c66d9ead6c
Charlie Root <root@galera-1.sea.darkain.info>
2025-12-16 04:14:40 +0000

    Updating 15.0-RELEASE upgrade script to use boot environments

--- 15-upgrade.sh
+++ 15-upgrade.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
 
+# CREATE BOOT ENVIRONMENT FOR 15.0-RELEASE
+bectl create pre-15.0-RELEASE
+bectl create 15.0-RELEASE
+mkdir /mnt/upgrade
+bectl mount 15.0-RELEASE /mnt/upgrade
+
+
 # CREATE PATH FOR TRUSTED KEY
-mkdir -p /usr/share/keys/pkgbase-15/trusted/
+mkdir -p /mnt/upgrade/usr/share/keys/pkgbase-15/trusted/
+
 
 # DOWNLOAD TRUSTED KEY FROM FREEBSD SRC MIRROR
-fetch -o /usr/share/keys/pkgbase-15/trusted/awskms-15 https://github.com/freebsd/freebsd-src/raw/refs/heads/main/share/keys/pkgbase-15/trusted/awskms-15
+fetch -o /mnt/upgrade/usr/share/keys/pkgbase-15/trusted/awskms-15 https://github.com/freebsd/freebsd-src/raw/refs/heads/main/share/keys/pkgbase-15/trusted/awskms-15
 
 
 # SETUP SOME VARIABLES TO MAKE LIFE A LITTLE EASIER
 OLD_PATH='/usr/share/keys/pkg'
 NEW_PATH='/usr/share/keys/pkgbase-15'
-FILE_PATH="/usr/local/etc/pkg/repos/FreeBSD-base.conf"
+FILE_PATH="/mnt/upgrade/usr/local/etc/pkg/repos/FreeBSD-base.conf"
+
 
 # UPDATE REPO
 sed -i.bak1 "s#  fingerprints: \"$OLD_PATH\"#  fingerprints: \"$NEW_PATH\"#" "$FILE_PATH"
@@ -19,4 +28,9 @@ sed -i.bak2 "s#base_release_3#base_release_0#" "$FILE_PATH"
 
 
 # UPGRADE FREEBSD!
-env ABI=FreeBSD:15:amd64 pkg-static upgrade -y -r FreeBSD-base
+env ABI=FreeBSD:15:amd64 pkg-static -c /mnt/upgrade upgrade -y -r FreeBSD-base
+
+
+# SWITCH TO 15.0-RELEASE
+bectl activate 15.0-RELEASE
+
