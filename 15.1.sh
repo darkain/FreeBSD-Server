65751ccb448eef656161c7aa6ce124a05ecf405a
Vince <git@darkain.com>
2026-07-21 18:41:37 -0700

    Add command to find .pkgnew files
    Added a command to find and list .pkgnew files that may need attention.

--- 15.1.sh
+++ 15.1.sh
@@ -11,3 +11,6 @@ pkg -oABI=FreeBSD:15:$(uname -p) -oOSVERSION=1501000 upgrade -r FreeBSD-base
 
 # KMODS TOO, JUST IN CASE
 pkg upgrade -r FreeBSD-ports-kmods
+
+# FILES CHANGED THAT MIGHT NEED ATTENTION
+find /etc /usr/local/etc -name '*.pkgnew' -ls
